38c46c2103c7a8b0343125fd4eff70de05e52e92
Leandro <leandrom.roldan@gmail.com>
2023-03-02 12:32:55 -0300

    change on static paths

--- pages/item/[itemId].tsx
+++ pages/item/[itemId].tsx
@@ -1,7 +1,7 @@
-import Head from "next/head";
-import { Layout } from "ui/layout";
-import { Item } from "components/item";
-import { fetchItemById } from "lib/api";
+import Head from 'next/head'
+import { Layout } from 'ui/layout'
+import { Item } from 'components/item'
+import { fetchItemById } from 'lib/api'
 
 export default function ItemPage({ data }: any) {
 	return (
@@ -15,32 +15,17 @@ export default function ItemPage({ data }: any) {
 				<Item itemData={data} />
 			</Layout>
 		</div>
-	);
+	)
 }
 
 export async function getStaticProps(context: any) {
-	const itemId = context.params.itemId;
-	const data = await fetchItemById(itemId as string);
-	return { props: { data }, revalidate: 10 };
+	const itemId = context.params.itemId
+	const data = await fetchItemById(itemId as string)
+	return { props: { data }, revalidate: 10 }
 }
 export async function getStaticPaths() {
 	return {
-		paths: [
-			{ params: { itemId: "receT4Ew4LOWySPRu" } },
-			{ params: { itemId: "recqOTGrGKPvuUQ9E" } },
-			{ params: { itemId: "reckSRhrkIg8YGVba" } },
-			{ params: { itemId: "recGKmXrIyM8FG64G" } },
-			{ params: { itemId: "recjM67d7CZtjQmxa" } },
-			{ params: { itemId: "recFGILR0jvxcNlID" } },
-			{ params: { itemId: "recSmwTjgHlCcj8ED" } },
-			{ params: { itemId: "recFvw7C8gw04BKV5" } },
-			{ params: { itemId: "rec19xw4A4ZcPjJpG" } },
-			{ params: { itemId: "recsPYyoRSHxyJM4A" } },
-			{ params: { itemId: "recTEm2YXFdDl0wPo" } },
-			{ params: { itemId: "reci4bavaKm8fOFrK" } },
-			{ params: { itemId: "recZrRriPxyX9IkZK" } },
-			{ params: { itemId: "recjXXxzLLIoyMF8u" } },
-		],
-		fallback: "blocking", // can also be false or 'blocking'
-	};
+		paths: [],
+		fallback: 'blocking',
+	}
 }
